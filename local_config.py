d1e30c32a7f657cf972c9ca26f5004f4c3cf3707
WoodrowtheFox <thnlundgren@gmail.com>
2026-09-23 15:46:50 -0500

    Need to still get the local machiene IP address and the DNS servers

--- local_config.py
+++ local_config.py
@@ -1,32 +1,42 @@
 from socket import *
 import requests
 import subprocess
-import time
-import random
 
-def get_IP():
+def get_public_IP():
     IP = requests.get('https://ifconfig.me')
     return IP.text
 
-def get_gateway():
+def get_ipconfig_data():
     gateway = subprocess.run("ipconfig /all", capture_output=True, text=True)
     with open("gateway.txt", "w") as file:
         file.write(gateway.stdout)
     #Getting the IPv4 of the Default Gateway
-    gateway = open("gateway.txt")
-    gaetwaydata = {};
-    in_block = 0;
-    is_next = 0;
+    gateway = open("gateway.txt", "r")
+    gatewaydata = {}
+    in_block = 0
+    ip_next = 0
     for line in gateway:
-        currline = line.split();
+        currline = line.split()
         if(in_block == 1):
-            if(is_next == 1):
-                gateway["IPv4"] = currline
-            if("Default" in currline):
-                is_next == 1;
-        if("Wireless" in currline):
+            if(ip_next == 1):
+                gatewaydata["Gateway"] = currline
+                ip_next = 0;
+                in_block = 0;
+            if("Gateway" in currline):
+                ip_next = 1;
+            if("Gateway" in currline):
+                gatewaydata["Gateway"] = currline
+            if("Media" in currline and "State" in currline):
+                if("disconnected" in currline):
+                    gatewaydata["Interface"] = "Wifi"
+                else:
+                    gatewaydata["Interface"] = "Ethernet"
+                in_block = 0;
+        if("Wireless" in currline and "Wi-Fi:" in currline):
             in_block = 1;
-    return gaetwaydata;
+        if("Ethernet" in currline and "2:" in currline):
+            in_block = 1;
+    return gatewaydata;
 
-print(get_IP())
-print(get_gateway())
+
+print(get_ipconfig_data())
