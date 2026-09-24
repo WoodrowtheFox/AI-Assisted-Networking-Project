e4fe436e8e17c51508d48c5327df5415c8d3cad9
WoodrowtheFox <thnlundgren@gmail.com>
2026-09-24 16:53:25 -0500

    Have the inital local_config file, will continute to tweak it

--- local_config.py
+++ local_config.py
@@ -7,36 +7,53 @@ def get_public_IP():
     return IP.text
 
 def get_ipconfig_data():
-    gateway = subprocess.run("ipconfig /all", capture_output=True, text=True)
-    with open("gateway.txt", "w") as file:
-        file.write(gateway.stdout)
+    configfiledata = {}
+    dns_data = {}
+    configfiledata["Public IP"] = get_public_IP()
+    configfile = subprocess.run("ipconfig /all", capture_output=True, text=True)
+    with open("configfile.txt", "w") as file:
+        file.write(configfile.stdout)
     #Getting the IPv4 of the Default Gateway
-    gateway = open("gateway.txt", "r")
-    gatewaydata = {}
+    configfile = open("configfile.txt", "r")
     in_block = 0
     ip_next = 0
-    for line in gateway:
+    dns = 0
+    for line in configfile:
         currline = line.split()
         if(in_block == 1):
             if(ip_next == 1):
-                gatewaydata["Gateway"] = currline
-                ip_next = 0;
-                in_block = 0;
+                for item in currline:
+                    configfiledata["Gateway"] =  item
+                ip_next = 0
+                in_block = 0
             if("Gateway" in currline):
-                ip_next = 1;
-            if("Gateway" in currline):
-                gatewaydata["Gateway"] = currline
+                ip_next = 1
             if("Media" in currline and "State" in currline):
                 if("disconnected" in currline):
-                    gatewaydata["Interface"] = "Wifi"
+                    configfiledata["Interface"] = "Wifi"
                 else:
-                    gatewaydata["Interface"] = "Ethernet"
-                in_block = 0;
+                    configfiledata["Interface"] = "Ethernet"
+                in_block = 0
         if("Wireless" in currline and "Wi-Fi:" in currline):
-            in_block = 1;
+            in_block = 1
         if("Ethernet" in currline and "2:" in currline):
-            in_block = 1;
-    return gatewaydata;
+            in_block = 1
+        if("IPv4" in currline):
+            currline = [word.replace("(Preferred)", "") for word in currline]
+            for item in currline:
+                configfiledata["Private IP"] = item
+        if(dns > 0 and "NetBIOS" not in currline):
+            dns += 1
+            for item in currline:
+                dns_data[dns] = item
+        if("NetBIOS" in currline):
+            dns = 0
+        if("DNS" in currline and "Servers" in currline):
+            dns = 1
+            for item in currline:
+                dns_data[dns] = item
+    configfiledata["DNS"] = dns_data
+    return configfiledata
 
 
 print(get_ipconfig_data())
