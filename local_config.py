35e11710f8cfee86e2ce879dfeb2113d888fe081
lundg273 <lundg273@d.umn.edu>
2026-09-24 20:36:29 -0500

    local_config is now done

--- local_config.py
+++ local_config.py
@@ -16,42 +16,44 @@ def get_ipconfig_data():
     #Getting the IPv4 of the Default Gateway
     configfile = open("configfile.txt", "r")
     in_block = 0
-    ip_next = 0
     dns = 0
+    ipnot = 1
     for line in configfile:
+        oldline = line
         currline = line.split()
         if(in_block == 1):
-            if(ip_next == 1):
+            if("IPv4" in currline):
+                currline = [word.replace("(Preferred)", "") for word in currline]
+                if(ipnot == 1):
+                    ipnot = 0
+                    for item in currline:
+                        currblock["Private IP"] = item
+            if(dns > 0 and "NetBIOS" not in currline):
+                dns += 1
                 for item in currline:
-                    configfiledata["Gateway"] =  item
-                ip_next = 0
-                in_block = 0
+                    dns_data[dns] = item
+            if("NetBIOS" in currline):
+                dns = 0
+            if("DNS" in currline and "Servers" in currline):
+                dns = 1
+                for item in currline:
+                    dns_data[dns] = item
             if("Gateway" in currline):
-                ip_next = 1
-            if("Media" in currline and "State" in currline):
-                if("disconnected" in currline):
-                    configfiledata["Interface"] = "Wifi"
-                else:
-                    configfiledata["Interface"] = "Ethernet"
-                in_block = 0
+                gatewayfail = ["Default", "Gateway", ".", ":"]
+                for item in currline:
+                    if(item not in gatewayfail):
+                        currblock["Gateway"] =  item
+                        configfiledata["NON-DNS"] = currblock
         if("Wireless" in currline and "Wi-Fi:" in currline):
             in_block = 1
-        if("Ethernet" in currline and "2:" in currline):
+            ipnot = 1
+            currblock = {}
+            currblock["Interface"] = "Wifi"
+        if("Ethernet" in currline and "adapter" in currline and oldline.rstrip().endswith(":") and oldline == oldline.lstrip()):
             in_block = 1
-        if("IPv4" in currline):
-            currline = [word.replace("(Preferred)", "") for word in currline]
-            for item in currline:
-                configfiledata["Private IP"] = item
-        if(dns > 0 and "NetBIOS" not in currline):
-            dns += 1
-            for item in currline:
-                dns_data[dns] = item
-        if("NetBIOS" in currline):
-            dns = 0
-        if("DNS" in currline and "Servers" in currline):
-            dns = 1
-            for item in currline:
-                dns_data[dns] = item
+            ipnot = 1
+            currblock = {}
+            currblock["Interface"] = "Ethernet"
     configfiledata["DNS"] = dns_data
     return configfiledata
 
